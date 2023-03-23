bb6d213406c8878de4b08552bb9a4e09f06f7f00
OSRoot <osamaessayed.el.4468@azhar.edu.eg>
2023-03-22 19:10:05 -0700

    Learned filtering

--- app.js
+++ app.js
@@ -20,17 +20,36 @@ const User = mongoose.model("Users", userSchema)
 
 // CRUD Operations 
 
-const getUsers = async () => {
-    try {
-        const data = await User.find({ firstname: "osroot" });
-        const data1 = await User.findById('641b9cf18c05b5f752c504c5');
-        const data2 = await User.find({}).where("firstname").equals("osroot");
-        console.log(data);
-        console.log(data1);
-        console.log(data2);
-    } catch (error) {
-        console.log(error);
-    }
+// const getUsers = async () => {
+//     try {
+//         const data = await User.find({ firstname: "osroot" });
+//         const data1 = await User.findById('641b9cf18c05b5f752c504c5');
+//         const data2 = await User.find({}).where("firstname").equals("osroot");
+//         console.log(data);
+//         console.log(data1);
+//         console.log(data2);
+//     } catch (error) {
+//         console.log(error);
+//     }
 
+// }
+// getUsers()
+
+
+const getAllUsers = async () => {
+    try {
+        // const data = await User.find({}).limit(5).sort({ firstname: 1 }) //  sorting decending
+        // const data = await User.find({}).limit(5).sort({ firstname: -1 }) //  sorting ascending
+        // const data = await User.find({}).limit(5).sort({ firstname: 1 }).select({ firstname: 1, lastname: 1 }) // sort and display the selected items only with the .select() method
+        // const data = await User.find({ firstname: /^A/ }).limit(5).sort({ firstname: -1 }) // use regx to find more specific item
+        // const data = await User.find({ firstname: /^A.*a$/ }).sort({ firstname: -1 }) // use regx to find more specific item
+        // const data = await User.find({ id: { $gt: 11 } }).sort({ firstname: 1 }) // filter using query comparison // filter by id where id greater than 11
+        // const data = await User.find({ id: { $gt: 11, $lt: 14 } }).sort({ firstname: -1 }) //  filter by id where id greater than 11 and less than 14
+        // const data = await User.find({}).or({ firstname: /A/, lastname: /Men/ }).sort({ firstname: -1 }).count()  //  filter by firstname and lastname who includes a string
+        const data = await User.find({}).and({ firstname: /s/, lastname: /.(an)$/ }).sort({ firstname: -1 }).select({ firstname: 1, lastname: 1 })  //  filter by firstname and lastname who includes a string
+        console.log(data);
+    } catch (error) {
+        console.log(error)
+    }
 }
-getUsers()
+getAllUsers()
